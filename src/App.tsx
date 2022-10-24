dec5effade2b4f61a119f54e1a3a968e925ff0a5
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-10-24 20:13:32 +0700

    done todos

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import ReminderList from './components/ReminderList';
 import Reminder from './models/reminder';
 import reminderService from './services/reminder';
+import NewReminder from './components/NewReminder';
 
 function App() {
   const [reminders, setReminders] = useState<Reminder[]>([]);
@@ -21,8 +22,14 @@ function App() {
     setReminders(reminders.filter((reminder) => reminder.id !== id));
   };
 
+  const addReminder = async (title: string) => {
+    const newReminder = await reminderService.addReminder(title);
+    setReminders([newReminder, ...reminders]);
+  };
+
   return (
     <div className='App'>
+      <NewReminder onAddReminder={addReminder} />
       <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   );
